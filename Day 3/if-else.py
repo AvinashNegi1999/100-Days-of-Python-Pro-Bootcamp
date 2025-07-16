de679cefc66c6ba26a26701f7c64c75c82092c7c
AvinashNegi1999 <avinashnegi1999work@gmail.com>
2025-07-16 07:57:44 +0530

    Create if-else.py

---
+++ Day 3/if-else.py
@@ -0,0 +1,7 @@
+print("welcome to rollercoaster ride")
+height=int(input("enter you height: "))
+
+if height>120:
+    print("you can go in rollercoaster")
+else:
+    print("sorry you have to grow taller before you can ride")
