06fb62201b3b604f87cfe9dbd45e90b243d2a26e
AvinashNegi1999 <avinashnegi1999work@gmail.com>
2025-07-16 15:53:11 +0530

    100 Days of Programming – Added new project

--- Day 3/rollercoaster ride.py
+++ Day 3/rollercoaster ride.py
@@ -1,26 +1,27 @@
-# program to give ticket for rollercoaster ride using if-else-elif conditions
-print("welcome to rollercoaster ride")
-height=int(input("enter you height: "))
-price=0
-if height>120:
-    print("you can go in rollercoaster")
-    age=int(input("enter you age: "))
-    if age<=12:
-        print("please pay 5$.")
-        price+=5
-    elif age<=18:
-        print("please pay 7$.")
-        price+=7
-    else:
-        print("please pay 12$")
-        price+=12
-else:
-    print("sorry you have to grow taller before you can ride.")
+# Program to give ticket for rollercoaster ride using if-else-elif conditions
 
-if price!=0:
-    picture=input("do you want to take a photo(y/n)")
-    if picture=="y":
-        price+=3
-        print(f"your total amount is {price}$")
+print("Welcome to the rollercoaster ride!")
+height = int(input("Enter your height in cm: "))
+price = 0
+
+if height > 120:
+    print("You can go on the rollercoaster.")
+    age = int(input("Enter your age: "))
+    
+    if age <= 12:
+        print("Please pay $5.")
+        price += 5
+    elif age <= 18:
+        print("Please pay $7.")
+        price += 7
     else:
-        print(f"your total amount is {price}$")
+        print("Please pay $12.")
+        price += 12
+
+    picture = input("Do you want to take a photo (y/n)? ").lower()
+    if picture == "y":
+        price += 3
+
+    print(f"Your final bill is ${price}.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
