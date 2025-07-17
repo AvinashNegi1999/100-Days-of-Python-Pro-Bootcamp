6a47416fc58a1d6069085072b5bbb3cac9824313
AvinashNegi1999 <avinashnegi1999work@gmail.com>
2025-07-17 11:39:52 +0530

    update

--- Day 3/rollercoaster ride.py
+++ Day 3/rollercoaster ride.py
@@ -1,4 +1,27 @@
-import random
+# Program to give ticket for rollercoaster ride using if-else-elif conditions
 
-random_number = random.randint(1, 10)
-print(random_number)
+print("Welcome to the rollercoaster ride!")
+height = int(input("Enter your height in cm: "))
+price = 0
+
+if height > 120:
+    print("You can go on the rollercoaster.")
+    age = int(input("Enter your age: "))
+    
+    if age <= 12:
+        print("Please pay $5.")
+        price += 5
+    elif age <= 18:
+        print("Please pay $7.")
+        price += 7
+    else:
+        print("Please pay $12.")
+        price += 12
+
+    picture = input("Do you want to take a photo (y/n)? ").lower()
+    if picture == "y":
+        price += 3
+
+    print(f"Your final bill is ${price}.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
--- Day 5/project.py
+++ Day 5/project.py
@@ -6,7 +6,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '?', '/', '\\', '{', '}', '[', ']']
 
-print("Welcome to the PyPassword Generator!")
+print("Welcome to the PyPassword Generator!\n")
 nr_letters=int(input("how many letters would you like in your password?\n"))
 nr_numbers=int(input("how many numbers would you like?\n"))
 nr_symbols=int(input("how many symbols would you like?\n"))
